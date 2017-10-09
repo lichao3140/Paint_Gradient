ee5f8e1abd7c7cdb2afde07b80ed499fbf1231f9
lichao3140 <396229938@qq.com>
2017-10-09 12:48:21 +0800

    放大镜效果

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
+# Paint的高级渲染
 ## 圆形头像 椭圆头像
-![Alt text](/screenshot/device-2017-10-09-111252.png)
+![圆形头像](/screenshot/device-2017-10-09-111252.png)
 ==
 ## 颜色线性变化
 ![颜色线性变化](/screenshot/device-2017-10-09-113009.png)
 ==
+## 放大镜效果
+![放大镜效果](/screenshot/device-2017-10-09-114727.png)
--- app/src/main/java/com/lqh/lichao/paint_gradient/MyGradientView.java
+++ app/src/main/java/com/lqh/lichao/paint_gradient/MyGradientView.java
@@ -5,12 +5,16 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ComposeShader;
 import android.graphics.LinearGradient;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.RadialGradient;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Shader;
+import android.graphics.SweepGradient;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
@@ -77,8 +81,38 @@ public class MyGradientView extends View {
          * float[] positions,数组大小跟colors数组一样大，中间依次摆放的几个颜色分别放置在那个位置上(参考比例从左往右)
          *    tile
          */
-        LinearGradient linearGradient = new LinearGradient(0, 0, 800, 800, mColors, null, Shader.TileMode.CLAMP);
+        //LinearGradient--线性渲染  霓虹灯文字，倒影图片
+        /*LinearGradient linearGradient = new LinearGradient(0, 0, 800, 800, mColors, null, Shader.TileMode.CLAMP);
         mPaint.setShader(linearGradient);
-        canvas.drawRect(0, 0, 800, 800, mPaint);
+        canvas.drawRect(0, 0, 800, 800, mPaint);*/
+
+        //RadialGradient----环形渲染 水波纹效果
+        /*RadialGradient radialGradient = new RadialGradient(300, 300, 100, mColors, null, Shader.TileMode.REPEAT);
+        mPaint.setShader(radialGradient);
+        canvas.drawCircle(300, 300, 300, mPaint);*/
+
+        //SweepGradient----渐变渲染/梯度渲染 雷达扫描效果
+        /*SweepGradient sweepGradient = new SweepGradient(300, 300, mColors, null);
+        mPaint.setShader(sweepGradient);
+        canvas.drawCircle(300, 300, 300, mPaint);*/
+
+        //ComposeShader----组合渲染
+        /*LinearGradient linearGradient = new LinearGradient(0, 0, 400, 400, mColors, null, Shader.TileMode.REPEAT);
+        BitmapShader bitmapShader = new BitmapShader(mBitmap, Shader.TileMode.CLAMP, Shader.TileMode.MIRROR);
+        ComposeShader mComposeShader = new ComposeShader(linearGradient, bitmapShader, PorterDuff.Mode.SRC_OVER);
+        mPaint.setShader(mComposeShader);
+        canvas.drawRect(0, 0, 800, 1000, mPaint);*/
+
+        /***************用ComposeShader即可实现心形图渐变效果*********************************/
+        //创建BitmapShader，用以绘制心
+        BitmapShader bitmapShader = new BitmapShader(mBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        //创建LinearGradient，用以产生从左上角到右下角的颜色渐变效果
+        LinearGradient linearGradient = new LinearGradient(0, 0, mWidth, mHeight, Color.GREEN, Color.BLUE, Shader.TileMode.CLAMP);
+        //bitmapShader对应目标像素，linearGradient对应源像素，像素颜色混合采用MULTIPLY模式
+        ComposeShader composeShader = new ComposeShader(bitmapShader, linearGradient, PorterDuff.Mode.MULTIPLY);
+        //将组合的composeShader作为画笔paint绘图所使用的shader
+        mPaint.setShader(composeShader);
+        //用composeShader绘制矩形区域
+        canvas.drawRect(0, 0, mWidth, mHeight, mPaint);
     }
 }
